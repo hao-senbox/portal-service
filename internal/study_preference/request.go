5e29fe548648f02ce6290a03c27f4e6b4b2c283b
provided <provided@provideds-Mac-mini.local>
2025-11-05 17:20:15 +0700

    push code

--- internal/study_preference/request.go
+++ internal/study_preference/request.go
@@ -4,7 +4,7 @@ type CreateStudyPreferenceRequest struct {
 	OrganizationID   string `json:"organization_id" bson:"organization_id" binding:"required"`
 	StudentID        string `json:"student_id" bson:"student_id" binding:"required"`
 	ParentSelection  []Data `json:"parent_selection" bson:"parent_selection"`
-	TeacherSelection []Data `json:"teacher_selection" bson:"teacher_selection" binding:"required"`
+	TeacherSelection []Data `json:"teacher_selections" bson:"teacher_selections" binding:"required"`
 }
 
 // UpdateStudyPreferenceRequest - Parent update their selection based on teacher's suggestion

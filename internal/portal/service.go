44b26d48caecb38316e579e0c18b97949142d5f1
provided <provided@provideds-Mac-mini.local>
2025-12-04 13:38:24 +0700

    push code

--- internal/portal/service.go
+++ internal/portal/service.go
@@ -393,7 +393,7 @@ func (s *portalService) generateToiletingStatistics(details []ActivityDetail) ma
 					if number1 == 0 {
 						number1 = 0
 					} else {
-						number1 -= 1
+						// number1 -= 1
 					}
 				} else if strings.Contains(value, "small") {
 					number1 += 1
@@ -406,7 +406,7 @@ func (s *portalService) generateToiletingStatistics(details []ActivityDetail) ma
 					if number2 == 0 {
 						number2 = 0
 					} else {
-						number2 -= 1
+						// number2 -= 1
 					}
 				} else if strings.Contains(value, "small") {
 					number2 += 1
@@ -419,7 +419,7 @@ func (s *portalService) generateToiletingStatistics(details []ActivityDetail) ma
 					if number3 == 0 {
 						number3 = 0
 					} else {
-						number3 -= 1
+						// number3 -= 1
 					}
 				} else if strings.Contains(value, "small") {
 					number3 += 1
